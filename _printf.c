5cf09f16ede05ee0e95079fe8a63753712e8b2ea
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-08-22 11:56:40 -0400

    close prans

--- _printf.c
+++ _printf.c
@@ -1,2 +1,40 @@
 #include "main.h"
 
+/**
+ * _printf - a function to print formatted strings
+ * @format: The accepted formats are c, s, i, d
+ * @return: The size of the string
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, len = 0;
+
+	if (!format || format == NULL)
+	{
+		return (-1);
+	}
+	va_start(args, format);
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			i++;
+			len++;
+		}
+		
+		if (format[i] == '\0')
+		{
+			return (len);
+		}
+	}
+}
+
+
+
+
+
+
+
+
